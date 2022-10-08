6311e83ed874fe1280d3739a7940edbba8fb65e7
lucasperez1993 <33763001+lucasperez1993@users.noreply.github.com>
2022-10-08 20:41:09 -0300

    1 file added

---
+++ ejercicios/06-desestructuracion-funciones.ts
@@ -0,0 +1,30 @@
+
+interface Producto {
+  desc: string;
+  precio: number;
+}
+
+const telefono: Producto = {
+  desc: 'Xiaomi Mi A3',
+  precio: 250,
+};
+
+const tablet: Producto = {
+  desc: 'Samsung Tab 7',
+  precio: 350,
+};
+
+function calcularISV(productos: Producto[]): [number, number] {
+  let total = 0;
+  productos.forEach(({ precio }) => {
+    total += precio;
+  });
+  return [total, total * 0.15];
+}
+
+const articulos = [telefono, tablet];
+
+const [total, isv] = calcularISV(articulos);
+
+console.log('Total: ', total);
+console.log('ISV: ', isv);
