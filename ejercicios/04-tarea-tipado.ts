2717648ce06f73962996137845cdb6133cf0c05e
lucasperez1993 <33763001+lucasperez1993@users.noreply.github.com>
2022-10-08 16:42:23 -0300

    1 file added

---
+++ ejercicios/04-tarea-tipado.ts
@@ -0,0 +1,38 @@
+interface SuperHeroe {
+  nombre: string;
+  edad: number;
+  direccion: Direccion;
+  mostrarDireccion: () => string;
+}
+
+interface Direccion {
+  calle: string;
+  pais: string;
+  ciudad: string;
+}
+
+const superHeroe: SuperHeroe = {
+  nombre: 'Spiderman',
+  edad: 30,
+  direccion: {
+    calle: 'Main St',
+    pais: 'USA',
+    ciudad: 'NY',
+  },
+  mostrarDireccion() {
+    return (
+      this.nombre +
+      ', ' +
+      ', ' +
+      this.direccion.pais +
+      ', ' +
+      this.direccion.ciudad
+    );
+  },
+};
+
+const direccion = superHeroe.mostrarDireccion();
+
+console.log(direccion);
+
+console.log(superHeroe.direccion.ciudad);
